3d9c522569841c2623a6a44478334be190714a5d
Melanija Racic <racicmelanija@gmail.com>
2021-12-29 12:29:45 +0100

    fix: Rename project and add dependencies

--- Backend/src/main/java/com/fishbook/BackendApplication.java
+++ Backend/src/main/java/com/fishbook/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.fishbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into country (code, name) values ('SRB', 'Srbija');
+insert into city (postal_code, name, country_id) values ('21000', 'Novi Sad', 1);
+insert into address (address, city_id, longitude, latitude) values ('Micurinova 17', 1, 0.0, 0.0)
+insert into role (name) values ('ROLE_CLIENT')
+insert into role (name) values ('ROLE_BOAT_OWNER')
+insert into role (name) values ('ROLE_INSTRUCTOR')
+insert into role (name) values ('ROLE_HOUSE_OWNER')
+insert into role (name) values ('ROLE_ADMIN')
+insert into users (first_name, last_name, email, password, phone_number, address_id, role_id, is_enabled, is_deleted) values ('Nikola', 'Nikolic', 'niki@gmail.com', 'pass', '12345', 1, 1, false, false);
