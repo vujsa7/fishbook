d1a7816bec64a4569f30c2246effcf6932e6aa5a
Melanija Racic <racicmelanija@gmail.com>
2021-12-30 09:34:52 +0100

    feat: Add registration request model class

---
+++ Backend/src/main/java/com/fishbook/registration/model/RegistrationRequest.java
@@ -0,0 +1,40 @@
+package com.fishbook.registration.model;
+
+import javax.persistence.*;
+
+@Entity
+public class RegistrationRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String registrationType;
+
+    @Column(nullable = false)
+    private String registrationMessage;
+}
