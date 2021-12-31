b2d2d8653389dc6f8fdf780f6acac37bf4d6842f
vujsa7 <vujisic27@gmail.com>
2021-12-31 13:41:03 +0100

    fix: Fix merge conflicts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationRequestsComponent } from './pages/registration-requests/registration-requests.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: '**', redirectTo: '' },
   { path: 'registration-requests', component: RegistrationRequestsComponent}
 ];
 
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { RegistrationRequestsComponent } from './pages/registration-requests/registration-requests.component';
+import { GlassContainerComponent } from './shared/components/glass-container/glass-container.component';
 import { PrimaryButtonComponent } from './shared/components/primary-button/primary-button.component';
 import { RegistrationTypePipe } from './shared/pipes/registration-type.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationRequestsComponent,
     PrimaryButtonComponent,
-    RegistrationTypePipe
+    RegistrationTypePipe,
+    HomeComponent,
+    RegisterComponent,
+    GlassContainerComponent
   ],
   imports: [
     BrowserModule,
