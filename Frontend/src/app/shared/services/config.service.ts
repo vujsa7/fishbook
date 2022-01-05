9ac3dcda23e97d4628fd6992b037099448f969b9
Melanija Racic <racicmelanija@gmail.com>
2022-01-05 12:43:58 +0100

    feat: Add model and service classes on frontend

---
+++ Frontend/src/app/shared/services/config.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GlobalConfig } from 'src/app/models/config/global-config.model';
+import { LoyaltyConfig } from 'src/app/models/config/loyalty-config.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+  private baseUrl: string = environment.baseUrl;
+  // For testing 
+  private header = new HttpHeaders()
+      .set('Authorization',  `Bearer eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJmaXNoYm9vayIsInN1YiI6Im5pa2lAZ21haWwuY29tIiwiYXVkIjoid2ViIiwiaWF0IjoxNjQxMzA2MjI4LCJleHAiOjE2NDEzMDgwMjh9.YeOpO8n6XIXjINTXOVbPAjjzJQxWVpki8Dgl8zI-rTYzeOd27aqEFjgAhDXB0HbwPpn28Du8CBs65NE8Snkzfw`)
+      .set('Content-Type', 'application/json')
+  
+  constructor(private http: HttpClient) { }
+
+  getLoyaltyConfig(): Observable<LoyaltyConfig[]> {
+    return this.http.get<LoyaltyConfig[]>(this.baseUrl + 'config/loyalty', {headers: this.header});
+  }
+
+  getGlobalConfig(): Observable<GlobalConfig> {
+    return this.http.get<GlobalConfig>(this.baseUrl + 'config/global', {headers: this.header});
+  }
+
+  updateLoyaltyConfig(loyaltyConfig: LoyaltyConfig): Observable<any> {
+    return this.http.put<any>(this.baseUrl + 'config/loyalty/' + loyaltyConfig.id, loyaltyConfig, {headers: this.header});
+  }
+
+  updateGlobalConfig(globalConfig: GlobalConfig): Observable<any> {
+    return this.http.put<any>(this.baseUrl + 'config/global/' + globalConfig.id, globalConfig, {headers: this.header});
+  }
+}
