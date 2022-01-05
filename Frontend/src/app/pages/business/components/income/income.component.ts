c3c57e03a3fd79d397e460c7f5ef881f0ea3d571
Melanija Racic <racicmelanija@gmail.com>
2022-01-05 13:20:30 +0100

    feat: Update system fee

--- Frontend/src/app/pages/business/components/income/income.component.ts
+++ Frontend/src/app/pages/business/components/income/income.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalConfig } from 'src/app/models/config/global-config.model';
+import { ConfigService } from 'src/app/shared/services/config.service';
 
 @Component({
   selector: 'app-income',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income.component.scss']
 })
 export class IncomeComponent implements OnInit {
+  globalConfig: GlobalConfig = new GlobalConfig(-1, -1, -1, -1);
+  isBtnDisabled: boolean = true;
 
-  constructor() { }
+  constructor(private configService: ConfigService) { }
 
   ngOnInit(): void {
+    this.configService.getGlobalConfig().subscribe(
+      data => this.globalConfig = data
+    )
   }
 
+  dataChanged(data: any): void {
+    this.isBtnDisabled = false;
+  }
+
+  updateGlobalConfig(): void {
+    this.configService.updateGlobalConfig(this.globalConfig).subscribe();
+    this.isBtnDisabled = true;
+  }
 }
