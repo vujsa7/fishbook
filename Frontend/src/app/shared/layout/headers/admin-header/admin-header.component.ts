1b2aeb7dec6fa339650493abc85a728220ed2d92
Melanija Racic <racicmelanija@gmail.com>
2022-03-29 17:43:35 +0200

    fix: Fix admin navbar

--- Frontend/src/app/shared/layout/headers/admin-header/admin-header.component.ts
+++ Frontend/src/app/shared/layout/headers/admin-header/admin-header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/authentication/auth.service';
 
 @Component({
   selector: 'app-admin-header',
@@ -10,8 +11,13 @@ export class AdminHeaderComponent implements OnInit {
   isVisible: boolean = true;
   dropdownMenuVisible: boolean = false;
   selectedButton: string = 'business';
+  adminName: string = '';
+  isSettingsVisible: boolean = false;
+  isGlassEffect: boolean = false;
+  _ = require('lodash');
+  debouncedOnScroll = this._.debounce(() => this.toggleNavigationBackground(), 300, {})
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
     router.events.subscribe(
       data => {
         if(this.router.url.includes("password-renewal") || this.router.url.includes("login") || this.router.url.includes("register")){
@@ -24,10 +30,32 @@ export class AdminHeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.adminName = this.authService.getTokenUsername();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.debouncedOnScroll();
   }
 
   toggleDropdownMenu(): void{
     this.dropdownMenuVisible = !this.dropdownMenuVisible;
+  }
+
+  toggleSettings(): void{
+    this.isSettingsVisible = !this.isSettingsVisible;
+  }
+
+  toggleNavigationBackground(): void{
+    if(window.pageYOffset == 0)
+      this.isGlassEffect = false;
+    else
+      this.isGlassEffect = true;
+  }
+
+  logout(): void{
+    this.authService.setToken("");
+    this.router.navigate(['/home']);
   }
 
   selectButton(selectedButton: string): void {
