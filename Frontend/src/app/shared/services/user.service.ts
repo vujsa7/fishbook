eb0e16ac4d1dce630255b25f8affd1e014d74cc6
Melanija Racic <racicmelanija@gmail.com>
2022-04-16 20:59:56 +0200

    feat: Update personal info

---
+++ Frontend/src/app/shared/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/core/authentication/auth.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  getUser() : Observable<any> {
+    return this.http.get<any>(this.baseUrl + 'users/' + this.authService.getTokenUsername(), { headers: this.authService.getHeader()});
+  }
+
+  updateUser(user: any) : Observable<any> {
+    return this.http.put<any>(this.baseUrl + 'users/' + this.authService.getTokenUsername(), user, { headers: this.authService.getHeader()});
+  }
+
+  updatePassword(newPassword: any) : Observable<any> {
+    return this.http.put<any>(this.baseUrl + 'users/' + this.authService.getTokenUsername() + '/password', newPassword, { headers: this.authService.getHeader()});
+  }
+}
